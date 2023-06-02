85d2ae2853f7cee56f46ab6214e058983cdf8839
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-06-02 11:12:31 +0700

    fetch

--- script.js
+++ script.js
@@ -1,36 +1,69 @@
-const frontendInput = document.getElementById("frontend")
+const getCourses = async () => {
+  try {
+    const response = await fetch("https://api.minireg.thanayut.in.th/courses")
+    // console.log(response)
 
-const frontendButton = document.getElementById("add-front")
+    const data = await response.json()
+    // console.log(data)
 
-const frontendMilestonesList = document.getElementById("frontend-milestone")
-
-frontendButton.addEventListener("click", (e) => {
-  e.preventDefault()
-  const newList = document.createElement("li")
-  // <li></li>
-  if (!frontendInput.value) {
-    alert("Please input something!")
-  } else {
-    newList.textContent = frontendInput.value
-    frontendMilestonesList.appendChild(newList)
+    data.courses.forEach((course) => {
+      console.log(course)
+    })
+  } catch (err) {
+    console.log(err)
   }
-  // <li>value</li>
-})
+}
 
-const backendInput = document.getElementById("backend")
+getCourses()
 
-const backendButton = document.getElementById("add-back")
+// data
+//   .then((res) => {
+//     console.log(res)
+//     data2
+//       .then((res) => {
+//         console.log(res)
+//       })
+//       .catch((err) => {
+//         console.log(err)
+//       })
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
 
-const backendMilestonesList = document.getElementById("backend-milestone")
+// const frontendInput = document.getElementById("frontend")
 
-backendButton.addEventListener("click", (e) => {
-  e.preventDefault()
-  const newList = document.createElement("li")
+// const frontendButton = document.getElementById("add-front")
 
-  if (!backendInput.value) {
-    alert("Please input something!")
-  } else {
-    newList.textContent = backendInput.value
-    backendMilestonesList.appendChild(newList)
-  }
-})
+// const frontendMilestonesList = document.getElementById("frontend-milestone")
+
+// frontendButton.addEventListener("click", (e) => {
+//   e.preventDefault()
+//   const newList = document.createElement("li")
+//   // <li></li>
+//   if (!frontendInput.value) {
+//     alert("Please input something!")
+//   } else {
+//     newList.textContent = frontendInput.value
+//     frontendMilestonesList.appendChild(newList)
+//   }
+//   // <li>value</li>
+// })
+
+// const backendInput = document.getElementById("backend")
+
+// const backendButton = document.getElementById("add-back")
+
+// const backendMilestonesList = document.getElementById("backend-milestone")
+
+// backendButton.addEventListener("click", (e) => {
+//   e.preventDefault()
+//   const newList = document.createElement("li")
+
+//   if (!backendInput.value) {
+//     alert("Please input something!")
+//   } else {
+//     newList.textContent = backendInput.value
+//     backendMilestonesList.appendChild(newList)
+//   }
+// })
